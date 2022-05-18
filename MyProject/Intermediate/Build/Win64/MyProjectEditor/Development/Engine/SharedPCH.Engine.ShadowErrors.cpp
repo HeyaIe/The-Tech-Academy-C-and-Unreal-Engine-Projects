bcbf49973ea0d8902b39c6228cf40f98521248b2
HeyaIe <bryngx@gmail.com>
2022-05-18 15:37:16 -0400

    Initial commit

---
+++ MyProject/Intermediate/Build/Win64/MyProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.30.30705
+#include "C:/Users/chawa/Documents/GitHub/The-Tech-Academy-CPP-Projects/MyProject/Intermediate/Build/Win64/MyProjectEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
