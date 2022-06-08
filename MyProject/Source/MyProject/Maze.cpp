7953aca3b3263fda92a62e667203720bf9d55ada
HeyaIe <bryngx@gmail.com>
2022-06-08 18:41:31 -0400

    Working on board movement

---
+++ MyProject/Source/MyProject/Maze.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Kismet/GameplayStatics.h"
+#include "Maze.h"
+
+// Sets default values
+AMaze::AMaze()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AMaze::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Maze = this->FindComponentByClass<UStaticMeshComponent>();
+	
+}
+
+// Called every frame
+void AMaze::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMaze::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(TEXT("TiltForward"), this, &AMaze::TiltForward);
+}
+
+void AMaze::TiltForward(float Value)
+{
+	FRotator Rotation(0);
+	Rotation.Pitch = Value * TurnSpeed * UGameplayStatics::GetWorldDeltaSeconds(this);
+
+	if (Maze)
+	{
+		AddActorWorldRotation(Rotation, true);
+	}
+
+}
+
